bb0b4ad11ef501fda517b3fdee5440720f3fa87f
Raymond Penners <raymond.penners@intenct.nl>
2016-01-21 09:39:44 +0100

    Indent HTML 2

--- init.el
+++ init.el
@@ -115,6 +115,11 @@
   (append flycheck-disabled-checkers
     '(javascript-jshint)))
 (flycheck-add-mode 'javascript-eslint 'web-mode)
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 ;; Python
 (require 'python)
